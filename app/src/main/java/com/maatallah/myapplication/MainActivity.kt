0b0b1cdcad8b88e261d540a6969201acab5bf06e
maatallah <maatallahabdelkader3@gmail.com>
2023-05-16 14:09:41 +0100

    Initial commit

---
+++ app/src/main/java/com/maatallah/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.maatallah.myapplication
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+}
+
+    fun showDropDown(view: View) {}
+}
